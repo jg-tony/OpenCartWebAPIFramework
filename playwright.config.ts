1cfa2acba1bcbbb15b7998660b9b0708162ab551
jg-tony <tony@automateandelevatebiz.com>
2026-09-14 15:48:36 -0500

    updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? "50%" : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
@@ -34,8 +34,8 @@ export default defineConfig({
     baseURL: process.env.BASE_URL, //"https://naveenautomationlabs.com/",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    headless: false,
     trace: "on-first-retry",
+    headless: !process.env.CI ? false : true,
   },
 
   /* Configure projects for major browsers */
